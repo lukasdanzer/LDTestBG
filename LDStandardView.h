8edb0333aacb46cdccd298e07650730544da3d6c
Lukas Danzer <lukas.danzer@jumio.com>
2014-11-24 12:00:22 +0100

    init

---
+++ LDStandardView.h
@@ -0,0 +1,13 @@
+//
+//  LDStandardView.h
+//  faceOverlaySample
+//
+//  Created by Lukas Danzer on 24/11/14.
+//  Copyright (c) 2014 Jumio Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LDStandardView : UIView
+
+@end
---
+++ LDStandardView.m
@@ -0,0 +1,21 @@
+//
+//  LDStandardView.m
+//  faceOverlaySample
+//
+//  Created by Lukas Danzer on 24/11/14.
+//  Copyright (c) 2014 Jumio Inc. All rights reserved.
+//
+
+#import "LDStandardView.h"
+
+@implementation LDStandardView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
